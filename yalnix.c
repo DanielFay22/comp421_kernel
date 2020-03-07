ce972d402e0faa8b5a577ee2e9f6ec3b231f59fd
DanielFay22 <dpf2@rice.edu>
2020-03-07 17:05:06 -0600

    This probably wont work at all

--- yalnix.c
+++ yalnix.c
@@ -1,45 +1,79 @@
 
-#include <comp421/yalnix.h>
+#include <stddef.h>
+
+#include "comp421/yalnix.h"
+#include "comp421/hardware.h"
+
+
+
+void trap_kernel_handler(ExceptionInfo *exceptionInfo);
+void trap_clock_handler(ExceptionInfo *exceptionInfo);
+void trap_illegal_handler(ExceptionInfo *exceptionInfo);
+void trap_memory_handler(ExceptionInfo *exceptionInfo);
+void trap_math_handler(ExceptionInfo *exceptionInfo);
+void trap_tty_transmit_handler(ExceptionInfo *exceptionInfo);
+void trap_tty_receive_handler(ExceptionInfo *exceptionInfo);
+
+
+
+void (*interrupt_table)(ExceptionInfo *)[TRAP_VECTOR_SIZE] = {NULL};
 
 
 
 
 
 
-int Fork(void) {
+void trap_kernel_handler(ExceptionInfo *exceptionInfo) {
+
+}
+
+void trap_clock_handler(ExceptionInfo *exceptionInfo) {
+
+}
+
+void trap_illegal_handler(ExceptionInfo *exceptionInfo) {
+
+}
+
+void trap_memory_handler(ExceptionInfo *exceptionInfo) {
+
+}
+
+void trap_math_handler(ExceptionInfo *exceptionInfo) {
+
+}
+
+void trap_tty_transmit_handler(ExceptionInfo *exceptionInfo) {
+
+}
+
+void trap_tty_receive_handler(ExceptionInfo *exceptionInfo) {
 
 }
 
 
-int Exec(char *filename, char **argvec) {
+
+
+
+
+void KernelStart(ExceptionInfo *info, unsigned int pmem_size,
+    void *orig_brk, char **cmd_args) {
+
+    // Setup interrupt vector table
+    interrupt_table[TRAP_KERNEL] = &trap_kernel_handler;
+    interrupt_table[TRAP_CLOCK] = &trap_clock_handler;
+    interrupt_table[TRAP_ILLEGAL] = &trap_illegal_handler;
+    interrupt_table[TRAP_MEMORY] = &trap_memory_handler;
+    interrupt_table[TRAP_MATH] = &trap_math_handler;
+    interrupt_table[TRAP_TTY_TRANSMIT] = &trap_tty_transmit_handler;
+    interrupt_table[TRAP_TTY_RECEIVE] = &trap_tty_receive_handler;
+
+    // Write address of interrupt vector table to REG_VECTOR_BASE register
+    (void (**)(ExceptionInfo *)) (REG_VECTOR_BASE) = interrupt_table;
+
+
 
 }
 
 
-void Exit(int status) {
 
-}
-
-int Wait(int *status_ptr) {
-
-}
-
-int GetPid(void) {
-
-}
-
-int Brk(void *addr) {
-
-}
-
-int Delay(int clock_ticks) {
-
-}
-
-int TtyRead(int tty_id, void *buf, int len) {
-
-}
-
-int TtyWrite(int tty_id, void *buf, int len) {
-
-}
